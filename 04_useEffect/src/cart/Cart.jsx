da953b46adb7b6d4c4d01c98b442098c4e7dcf43
@FajluRahaman56 <fajlurahaman185@gmail.com>
2026-07-21 10:01:20 +0530

    "initial update"

---
+++ 04_useEffect/src/cart/Cart.jsx
@@ -0,0 +1,28 @@
+import  { useEffect, useState } from 'react'
+import { axios } from 'axios';
+
+export default function Cart() {
+    const [cartList, setcartList]  = useState();
+
+    useEffect(() => {
+        const fetchCart = async() => {
+            let carts = await axios.get("https://fakestoreapi.com/carts");
+            let cartsData = carts.data;
+            setcartList(cartsData);
+        }
+        fetchCart();
+    }, [])
+  return (
+    <div>
+        {cartList?.map((curCart) => {
+            return (
+                <ul className='list-group  w-50' key = {curCart.id}>
+                    <li className='list-group-item'>User Id: {curCart.userId}</li>
+                </ul>
+            )
+        })
+
+        }
+    </div>
+  )
+}
